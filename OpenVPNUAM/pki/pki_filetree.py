f27a251454b2d2d9a884464bd8c05ed86f127256
Turgon37 <pgindraud+github@gmail.com>
2015-10-10 15:21:26 +0200

    Move pki file to an python package
    Add a PKI File Tree to manage all file in the file system
    Add a function to generate new certificate
    
    TODO => make a call to database to retrieve a valid new certificate

---
+++ OpenVPNUAM/pki/pki_filetree.py
@@ -0,0 +1,154 @@
+# -*- coding: utf8 -*-
+
+# This file is a part of OpenVPN-UAM
+#
+# Copyright (c) 2015 Thomas PAJON, Pierre GINDRAUD
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""PKI - Public Key Infrastructure File Tree program class
+
+This class is responsive of management of all SSL files
+"""
+
+# System imports
+import logging
+import os
+
+import OpenSSL
+from OpenSSL import crypto
+
+# Project imports
+from ..config import Error
+
+# Global project declarations
+g_sys_log = logging.getLogger('openvpn-uam.pki.file')
+
+
+class PKIFileTree(object):
+  """Build an instance of the pki model class
+
+  This instance must be called in the openvpn uam program class
+  """
+
+  def __init__(self, confparser):
+    """Constructor : Build a new PKI API instance
+    """
+    self.__cp = confparser
+    # the root path of file tree
+    self.__new_cert_directory = "certificates/"
+
+  def load(self):
+    """Return a boolean indicates if PKI is ready to work or not
+
+    This function check things required by PKI working and return a boolean
+    that indicates if the PKI is ready to work with certificate or not
+    @return [bool] The ready status
+    """
+
+    # check PKI section in configuration file
+    if not self.__cp.has_section(self.__cp.PKI_SECTION):
+      g_sys_log.error('Missing pki section in configuration file')
+      return False
+    sec = self.__cp.getItems(self.__cp.PKI_SECTION)
+
+    # read the new cert directory path from config file
+    self.__new_cert_directory = self.__cp.get(
+        self.__cp.PKI_SECTION,
+        'cert_directory',
+        fallback=self.__new_cert_directory).rstrip('/') + '/'
+
+    if not self.makePath(self.__new_cert_directory):
+      g_sys_log.fatal("Certificate directory is invalid")
+      return False
+    return True
+
+# Tools
+  def makePath(self, path):
+    """Ensure that the given path is builded on the file system
+
+    @param path [str] the path to check for
+    @return [bool] True if the entire path is existing on the FS
+                   False if an error happen
+    """
+    p = ""
+    for folder in path.split('/'):
+      if len(folder) == 0:
+        continue
+      p += folder + '/'
+      if not os.path.exists(p):
+        # create it
+        g_sys_log.info("Creating directory '%s'", p)
+        try:
+          os.mkdir(p)
+        except OSError as e:
+          g_sys_log.error("File '%s' already exist", p)
+          return False
+      # if cert path already exist
+      else:
+        # check if it is a valid directory
+        if not os.path.isdir(p):
+          g_sys_log.error("File '%s' is not a directory", p)
+          return False
+    return True
+
+# API
+  def storeBytesToFile(self, content, path):
+    """Write a list of bytes into a file
+
+    @param content [bytes/str] the content to write into the file
+    @param path [str] the path to the file into
+    """
+    f = None
+    if os.path.exists(path):
+      g_sys_log.error("Error during export of file '%s'.", path)
+      return
+
+    if isinstance(content, bytes):
+      # open output file in binary mode
+      f = open(path, "wb")
+    elif isinstance(content, str):
+      # open output file in text mode
+      f = open(path, "wt")
+    assert f is not None
+
+    f.write(content)
+    f.close()
+
+  def storePKIUserCertificate(self, user, hostname, certificate, obj):
+    """Store a given PKI object into a file
+
+    @param user [User] the user to which the certificate is associated
+    @param hostname [Hostname] the hostname to which the certificate is
+                              associated
+    @param obj [X509/PKey] The object that will be dump to the file
+    """
+    path = (self.__new_cert_directory + str(user.id) + "/" + str(hostname.id) +
+            "/")
+    self.makePath(path)
+
+    bytes_ = None
+    if isinstance(obj, OpenSSL.crypto.X509):
+      bytes_ = crypto.dump_certificate(crypto.FILETYPE_PEM, obj)
+      path += str(certificate.id) + ".crt"
+    elif isinstance(obj, OpenSSL.crypto.PKey):
+      bytes_ = crypto.dump_privatekey(crypto.FILETYPE_PEM, obj)
+      path += str(certificate.id) + ".key"
+    assert bytes_ is not None
+    self.storeBytesToFile(bytes_, path)
--- OpenVPNUAM/pki/pki_manager.py
+++ OpenVPNUAM/pki/pki_manager.py
@@ -26,6 +26,7 @@
 
 This class is in charge of management of all certificates operations
 renewal and also their revocation.
+This is the entry point of all SSL operations
 """
 
 # System imports
@@ -36,15 +37,16 @@ import datetime
 try:
   import OpenSSL
   from OpenSSL import crypto
-  #OpenSSL.crypto._lib.OPENSSL_config(b"openssl.cn")
-
   from OpenSSL import SSL
+  #OpenSSL.crypto._lib.OPENSSL_config(b"openssl.cn")
 except ImportError:
   raise Exception("Module OpenSSL required " +
                   " https://pypi.python.org/pypi/pyOpenSSL")
 
 # Project imports
-from .config import Error
+from .pki_filetree import PKIFileTree
+from .. import models as Model
+from ..config import Error
 
 # Global project declarations
 g_sys_log = logging.getLogger('openvpn-uam.pki')
@@ -60,6 +62,8 @@ class PublicKeyInfrastructure(object):
     """Constructor : Build a new PKI API instance
     """
     self.__cp = confparser
+    # the file tree to use to store all file
+    self.__ft = PKIFileTree(confparser)
     # path to CA cert
     self.__certificate_authority = None
     # path to CA key
@@ -68,8 +72,7 @@ class PublicKeyInfrastructure(object):
     # Path to the server certificate, use to ensure that this certificate is
     # still valid
     self.__server_certificate = None
-    # This directory will contains newly created certificates
-    self.__new_cert_directory = "certificates/"
+    # number of bits for new private key
     self.__cert_key_size = int(2048)
 
   def load(self):
@@ -83,37 +86,16 @@ class PublicKeyInfrastructure(object):
                    SSL.SSLeay_version(SSL.SSLEAY_VERSION).decode())
     # check PKI section in configuration file
     if not self.__cp.has_section(self.__cp.PKI_SECTION):
-      g_sys_log.error('Missing pki section in configuration file')
+      g_sys_log.fatal('Missing pki section in configuration file')
       return False
-    sec = self.__cp.getItems(self.__cp.PKI_SECTION)
-
-    # read the new cert directory path from config file
-    self.__new_cert_directory = self.__cp.get(
-        self.__cp.PKI_SECTION,
-        'cert_directory',
-        fallback=self.__new_cert_directory)
+    if not self.__ft.load():
+      g_sys_log.fatal('Enabled to load File Manager for PKI')
+      return False
 
     self.__cert_key_size = self.__cp.getint(
         self.__cp.PKI_SECTION,
         'cert_key_size',
         fallback=self.__cert_key_size)
-
-    # if cert path doesn't exist
-    if not os.path.exists(self.__new_cert_directory):
-      # create it
-      g_sys_log.info('Creating directory "%s" for new certificate',
-                     self.__new_cert_directory)
-      try:
-        os.mkdir(self.__new_cert_directory)
-      except OSError as e:
-        g_sys_log.error('File "%s" already exist', self.__new_cert_directory)
-        return False
-    # if cert path already exist
-    else:
-      # check if it is a valid directory
-      if not os.path.isdir(self.__new_cert_directory):
-        g_sys_log.error('Certificate directory is invalid')
-        return False
 
     try:
       self.__certificate_authority = self.loadCertificate(
@@ -131,15 +113,14 @@ class PublicKeyInfrastructure(object):
       g_sys_log.error('CA Certificate is missing')
       return False
     else:
-      g_sys_log.info('Using CA Certificate "%s"',
+      g_sys_log.info("Using CA Certificate '%s'",
                      self.__certificate_authority.get_subject().CN)
     if self.__certificate_authority_key is None:
       g_sys_log.error('CA Key is missing')
       return False
     else:
-      g_sys_log.info('Using CA Private Key with size "%s" bits',
+      g_sys_log.info("Using CA Private Key with size '%s' bits",
                      self.__certificate_authority_key.bits())
-
     return True
 
   def checkRequirements(self):
@@ -226,6 +207,7 @@ class PublicKeyInfrastructure(object):
     """
     g_sys_log.debug("Building a new certificate for Hostname(%s) '%s'",
                     hostname.id, hostname.name)
+
     # build a new key for the given username
     g_sys_log.debug("Generate a %s bits RSA Private Key", self.__cert_key_size)
     key = OpenSSL.crypto.PKey()
@@ -241,7 +223,7 @@ class PublicKeyInfrastructure(object):
     cert.get_subject().CN = user.cuid + "_" + hostname.name
     cert.get_subject().emailAddress = user.user_mail
     cert.get_subject().name = (user.cuid + "_" + hostname.name + "_" +
-                              str(datetime.datetime.today()) )
+                               str(datetime.datetime.today()))
 
     cert.set_notBefore(b"20000101000000Z")
     cert.set_notAfter(b"20200101000000Z")
@@ -251,12 +233,7 @@ class PublicKeyInfrastructure(object):
     cert.set_pubkey(key)
     #cert.sign(cakey, "sha1")
 
-    # open output file in binary mode
-    file_key = open("rsa.key", "wb")
-    file_key.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
-    file_key.close()
-
-    # open output file in binary mode
-    file_cert = open("rsa.crt", "wb")
-    file_cert.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
-    file_cert.close()
+    m_cert = Model.Certificate(cert.get_notBefore(), cert.get_notAfter())
+    
+    self.__ft.storePKIUserCertificate(user, hostname, m_cert, key)
+    self.__ft.storePKIUserCertificate(user, hostname, m_cert, cert)
